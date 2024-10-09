953019090c69f56b8d79a2619e890d65a80434f7
Stephen DeBaun <sdebaun74@gmail.com>
2024-10-09 08:03:49 -0700

    hello world http server

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { HttpRouter, HttpServer, HttpServerResponse } from "@effect/platform";
+import { BunHttpServer, BunRuntime } from "@effect/platform-bun";
+import { Layer } from "effect";
+
+const router = HttpRouter.empty.pipe(
+  HttpRouter.get("/", HttpServerResponse.text("Hello World"))
+)
+
+const app = router.pipe(HttpServer.serve(), HttpServer.withLogAddress)
+
+const port = 3000
+
+const ServerLive = BunHttpServer.layer({ port })
+
+BunRuntime.runMain(Layer.launch(Layer.provide(app, ServerLive)))
